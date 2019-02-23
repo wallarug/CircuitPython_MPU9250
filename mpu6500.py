c6ef85de3b24a4d730e2ccb0c45d1556d9f675a8
Eike Welk <eike.welk@gmx.net>
2019-02-23 13:25:42 +0100

    Create separate read and write functions for I2C.

--- mpu6500.py
+++ mpu6500.py
@@ -24,51 +24,52 @@
 MicroPython I2C driver for MPU6500 6-axis motion tracking device
 """
 
-__version__ = "0.3.0-dev"
+__version__ = "0.1.0-a"
 
 # pylint: disable=import-error
 import ustruct
 import utime
-from machine import I2C, Pin
-from micropython import const
+#from machine import I2C, Pin
+import Adafruit_GPIO.I2C
+#from micropython import const
 # pylint: enable=import-error
 
-_GYRO_CONFIG = const(0x1b)
-_ACCEL_CONFIG = const(0x1c)
-_ACCEL_CONFIG2 = const(0x1d)
-_INT_PIN_CFG = const(0x37)
-_ACCEL_XOUT_H = const(0x3b)
-_ACCEL_XOUT_L = const(0x3c)
-_ACCEL_YOUT_H = const(0x3d)
-_ACCEL_YOUT_L = const(0x3e)
-_ACCEL_ZOUT_H = const(0x3f)
-_ACCEL_ZOUT_L= const(0x40)
-_TEMP_OUT_H = const(0x41)
-_TEMP_OUT_L = const(0x42)
-_GYRO_XOUT_H = const(0x43)
-_GYRO_XOUT_L = const(0x44)
-_GYRO_YOUT_H = const(0x45)
-_GYRO_YOUT_L = const(0x46)
-_GYRO_ZOUT_H = const(0x47)
-_GYRO_ZOUT_L = const(0x48)
-_WHO_AM_I = const(0x75)
+_GYRO_CONFIG = 0x1b
+_ACCEL_CONFIG = 0x1c
+_ACCEL_CONFIG2 = 0x1d
+_INT_PIN_CFG = 0x37
+_ACCEL_XOUT_H = 0x3b
+_ACCEL_XOUT_L = 0x3c
+_ACCEL_YOUT_H = 0x3d
+_ACCEL_YOUT_L = 0x3e
+_ACCEL_ZOUT_H = 0x3f
+_ACCEL_ZOUT_L= 0x40
+_TEMP_OUT_H = 0x41
+_TEMP_OUT_L = 0x42
+_GYRO_XOUT_H = 0x43
+_GYRO_XOUT_L = 0x44
+_GYRO_YOUT_H = 0x45
+_GYRO_YOUT_L = 0x46
+_GYRO_ZOUT_H = 0x47
+_GYRO_ZOUT_L = 0x48
+_WHO_AM_I = 0x75
 
-#_ACCEL_FS_MASK = const(0b00011000)
-ACCEL_FS_SEL_2G = const(0b00000000)
-ACCEL_FS_SEL_4G = const(0b00001000)
-ACCEL_FS_SEL_8G = const(0b00010000)
-ACCEL_FS_SEL_16G = const(0b00011000)
+#_ACCEL_FS_MASK = 0b00011000
+ACCEL_FS_SEL_2G = 0b00000000
+ACCEL_FS_SEL_4G = 0b00001000
+ACCEL_FS_SEL_8G = 0b00010000
+ACCEL_FS_SEL_16G = 0b00011000
 
 _ACCEL_SO_2G = 16384 # 1 / 16384 ie. 0.061 mg / digit
 _ACCEL_SO_4G = 8192 # 1 / 8192 ie. 0.122 mg / digit
 _ACCEL_SO_8G = 4096 # 1 / 4096 ie. 0.244 mg / digit
 _ACCEL_SO_16G = 2048 # 1 / 2048 ie. 0.488 mg / digit
 
-#_GYRO_FS_MASK = const(0b00011000)
-GYRO_FS_SEL_250DPS = const(0b00000000)
-GYRO_FS_SEL_500DPS = const(0b00001000)
-GYRO_FS_SEL_1000DPS = const(0b00010000)
-GYRO_FS_SEL_2000DPS = const(0b00011000)
+#_GYRO_FS_MASK = 0b00011000
+GYRO_FS_SEL_250DPS = 0b00000000
+GYRO_FS_SEL_500DPS = 0b00001000
+GYRO_FS_SEL_1000DPS = 0b00010000
+GYRO_FS_SEL_2000DPS = 0b00011000
 
 _GYRO_SO_250DPS = 131
 _GYRO_SO_500DPS = 62.5
@@ -79,9 +80,9 @@ _TEMP_SO = 333.87
 _TEMP_OFFSET = 21
 
 # Used for enablind and disabling the i2c bypass access
-_I2C_BYPASS_MASK = const(0b00000010)
-_I2C_BYPASS_EN = const(0b00000010)
-_I2C_BYPASS_DIS = const(0b00000000)
+_I2C_BYPASS_MASK = 0b00000010
+_I2C_BYPASS_EN = 0b00000010
+_I2C_BYPASS_DIS = 0b00000000
 
 SF_G = 1
 SF_M_S2 = 9.80665 # 1 g = 9.80665 m/s2 ie. standard gravity
@@ -91,12 +92,20 @@ SF_RAD_S = 0.017453292519943 # 1 deg/s is 0.017453292519943 rad/s
 class MPU6500:
     """Class which provides interface to MPU6500 6-axis motion tracking device."""
     def __init__(
-        self, i2c, address=0x68,
+        self, i2c_interface=None, busnum=1, address=0x68,
         accel_fs=ACCEL_FS_SEL_2G, gyro_fs=GYRO_FS_SEL_250DPS,
         accel_sf=SF_M_S2, gyro_sf=SF_RAD_S,
         gyro_offset=(0, 0, 0)
     ):
-        self.i2c = i2c
+        if i2c_interface is None:
+            # Use pure python I2C interface if none is specified.
+            import Adafruit_PureIO.smbus
+            self.i2c = Adafruit_PureIO.smbus.SMBus(busnum)
+        else:
+            # Otherwise use the provided class to create an smbus interface.
+            self.i2c = i2c_interface(busnum)
+
+        #self.i2c = i2c
         self.address = address
 
         if 0x71 != self.whoami:
@@ -109,10 +118,10 @@ class MPU6500:
         self._gyro_offset = gyro_offset
 
         # Enable I2C bypass to access for MPU9250 magnetometer access.
-        char = self._register_char(_INT_PIN_CFG)
+        char = self._read_register_char(_INT_PIN_CFG)
         char &= ~_I2C_BYPASS_MASK # clear I2C bits
         char |= _I2C_BYPASS_EN
-        self._register_char(_INT_PIN_CFG, char)
+        self._write_register_char(_INT_PIN_CFG, char)
 
     @property
     def acceleration(self):
@@ -125,7 +134,7 @@ class MPU6500:
         so = self._accel_so
         sf = self._accel_sf
 
-        xyz = self._register_three_shorts(_ACCEL_XOUT_H)
+        xyz = self._read_register_three_shorts(_ACCEL_XOUT_H)
         return tuple([value / so * sf for value in xyz])
 
     @property
@@ -137,7 +146,7 @@ class MPU6500:
         sf = self._gyro_sf
         ox, oy, oz = self._gyro_offset
 
-        xyz = self._register_three_shorts(_GYRO_XOUT_H)
+        xyz = self._read_register_three_shorts(_GYRO_XOUT_H)
         xyz = [value / so * sf for value in xyz]
 
         xyz[0] -= ox
@@ -151,15 +160,15 @@ class MPU6500:
         """
         Die temperature in celcius as a float.
         """
-        temp = self._register_short(_TEMP_OUT_H)
+        temp = self._read_register_short(_TEMP_OUT_H)
         return ((temp - _TEMP_OFFSET) / _TEMP_SO) + _TEMP_OFFSET
 
     @property
     def whoami(self):
         """ Value of the whoami register. """
-        return self._register_char(_WHO_AM_I)
+        return self._read_register_char(_WHO_AM_I)
 
-    def calibrate(self, count=256, delay=0):
+    def calibrate_gyro(self, count=256, delay=0):
         ox, oy, oz = (0.0, 0.0, 0.0)
         self._gyro_offset = (0.0, 0.0, 0.0)
         n = float(count)
@@ -175,28 +184,28 @@ class MPU6500:
         self._gyro_offset = (ox / n, oy / n, oz / n)
         return self._gyro_offset
 
-    def _register_short(self, register, value=None, buf=bytearray(2)):
-        if value is None:
-            self.i2c.readfrom_mem_into(self.address, register, buf)
-            return ustruct.unpack(">h", buf)[0]
+    def _read_register_short(self, register, buf=bytearray(2)):
+        self.i2c.readfrom_mem_into(self.address, register, buf)
+        return ustruct.unpack(">h", buf)[0]
 
+    def _write_register_short(self, register, value, buf=bytearray(2)):
         ustruct.pack_into(">h", buf, 0, value)
         return self.i2c.writeto_mem(self.address, register, buf)
 
-    def _register_three_shorts(self, register, buf=bytearray(6)):
+    def _read_register_three_shorts(self, register, buf=bytearray(6)):
         self.i2c.readfrom_mem_into(self.address, register, buf)
         return ustruct.unpack(">hhh", buf)
 
-    def _register_char(self, register, value=None, buf=bytearray(1)):
-        if value is None:
-            self.i2c.readfrom_mem_into(self.address, register, buf)
-            return buf[0]
+    def _read_register_char(self, register, buf=bytearray(1)):
+        self.i2c.readfrom_mem_into(self.address, register, buf)
+        return buf[0]
 
+    def _write_register_char(self, register, value, buf=bytearray(1)):
         ustruct.pack_into("<b", buf, 0, value)
         return self.i2c.writeto_mem(self.address, register, buf)
 
     def _accel_fs(self, value):
-        self._register_char(_ACCEL_CONFIG, value)
+        self._write_register_char(_ACCEL_CONFIG, value)
 
         # Return the sensitivity divider
         if ACCEL_FS_SEL_2G == value:
@@ -209,7 +218,7 @@ class MPU6500:
             return _ACCEL_SO_16G
 
     def _gyro_fs(self, value):
-        self._register_char(_GYRO_CONFIG, value)
+        self._write_register_char(_GYRO_CONFIG, value)
 
         # Return the sensitivity divider
         if GYRO_FS_SEL_250DPS == value:
