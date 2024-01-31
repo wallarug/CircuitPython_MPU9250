7e282c852c205ebab6a886281e2accba9b1836e7
Davy39 <davy@ik.me>
2024-01-31 17:31:50 +0100

    Remove adafruit_bus_device.i2c_device.I2CDevice.write(stop=False)

--- robohat_mpu9250/mpu6500.py
+++ robohat_mpu9250/mpu6500.py
@@ -230,7 +230,7 @@ class MPU6500:
         device = self.i2c
         with device as i2c:
             self._BUFFER[0] = address & 0xFF
-            i2c.write(self._BUFFER, end=1, stop=False)
+            i2c.write(self._BUFFER, end=1)
             i2c.readinto(self._BUFFER, end=1)
         return self._BUFFER[0]
 
@@ -238,7 +238,7 @@ class MPU6500:
         device = self.i2c
         with device as i2c:
             self._BUFFER[0] = address & 0xFF
-            i2c.write(self._BUFFER, end=1, stop=False)
+            i2c.write(self._BUFFER, end=1)
             i2c.readinto(self._BUFFER, end=count)
         if count == 2:
             buf = bytearray(4)
